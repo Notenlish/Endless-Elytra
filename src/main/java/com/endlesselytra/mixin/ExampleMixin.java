c2ca59ab3fc11a47f9883a12857cbfc600a574b1
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-07-30 13:18:40 +0300

    1.21.10 port

--- src/main/java/com/endlesselytra/mixin/ExampleMixin.java
+++ src/main/java/com/endlesselytra/mixin/ExampleMixin.java
@@ -1,11 +1,9 @@
 package com.endlesselytra.mixin;
 
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,10 +14,12 @@ import java.util.function.Consumer;
 @Mixin(ItemStack.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "applyDamage", cancellable = true)
-	private void preventElytraDamage(int newDamage,
-	                                 @Nullable ServerPlayer player,
-	                                 Consumer<Item> onBreak,
-	                                 CallbackInfo ci) {
+	private void preventElytraDamage(
+			int newDamage,
+			ServerPlayer player,
+			Consumer<Item> onBreak,
+			CallbackInfo ci
+	) {
 		if (((ItemStack) (Object) this).is(Items.ELYTRA)) {
 			ci.cancel();
 		}
